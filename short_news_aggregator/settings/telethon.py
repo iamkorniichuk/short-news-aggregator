1d9a1b25ffc6dab9dd247776237e4b47ac6f4baa
iamkorniichuk <iamkorniichuk@gmail.com>
2025-02-23 23:00:24 +0300

    refactor: migrate to Python 3.12

--- short_news_aggregator/settings/telethon.py
+++ short_news_aggregator/settings/telethon.py
@@ -3,7 +3,7 @@ from .base import env
 
 with env.prefixed("TELEGRAM_"):
     TELETHON = {
-        "API_ID": env.str("API_ID"),
+        "API_ID": env.int("API_ID"),
         "API_HASH": env.str("API_HASH"),
         "DEFAULT_PHONE_NUMBER": env.str("DEFAULT_PHONE_NUMBER"),
     }
